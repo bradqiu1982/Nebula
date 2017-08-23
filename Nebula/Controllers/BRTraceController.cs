63b45964b19e5a35f1c92bd7d23d014683cfd595
Brad Qiu <brad.qiu@finisar.com>
2017-08-23 16:58:39 +0800

    make BR updata ,query rule

---
+++ Nebula/Controllers/BRTraceController.cs
@@ -0,0 +1,107 @@
+﻿using Nebula.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nebula.Controllers
+{
+    public class BRTraceController : Controller
+    {
+
+        public void LoadNewBR()
+        {
+            string datestring = DateTime.Now.ToString("yyyyMMdd");
+            string datefolder = Server.MapPath("~/userfiles") + "\\docs\\" + datestring + "\\";
+
+            if (!Directory.Exists(datefolder))
+            {
+                Directory.CreateDirectory(datefolder);
+            }
+
+            var updatedfolder = datefolder + "agileupdated";
+            var newqueryfolder = datefolder + "agilenewqueried";
+
+            if (Directory.Exists(updatedfolder) 
+                && !Directory.Exists(newqueryfolder))
+            {
+                var syscfgdict = NebulaDataCollector.GetSysConfig(this);
+                var AGILEURL = syscfgdict["AGILEURL"];
+                var LOCALSITEPORT = syscfgdict["LOCALSITEPORT"];
+                var SAVELOCATION = (Server.MapPath("~/userfiles") + "\\docs\\").Replace("\\", "/");
+                var PMNames = syscfgdict["TRACEPM"];
+                var FirstTraceTime = syscfgdict["FIRSTTRACETIME"];
+
+                AgileVM.RetrieveNewBR(AGILEURL, LOCALSITEPORT, SAVELOCATION, PMNames, FirstTraceTime);
+            }
+            
+        }
+
+        public void UpdateExistBR()
+        {
+            string datestring = DateTime.Now.ToString("yyyyMMdd");
+            string datefolder = Server.MapPath("~/userfiles") + "\\docs\\" + datestring + "\\";
+
+            if (!Directory.Exists(datefolder))
+            {
+                Directory.CreateDirectory(datefolder);
+
+                var syscfgdict = NebulaDataCollector.GetSysConfig(this);
+                var AGILEURL = syscfgdict["AGILEURL"];
+                var LOCALSITEPORT = syscfgdict["LOCALSITEPORT"];
+                var SAVELOCATION = (Server.MapPath("~/userfiles") + "\\docs\\").Replace("\\", "/");
+                AgileVM.UpdateExistBR(AGILEURL, LOCALSITEPORT, SAVELOCATION);
+            }
+        }
+
+        public ActionResult HeartBeat()
+        {
+            UpdateExistBR();
+            LoadNewBR();
+            return View();
+        }
+
+        public ActionResult NewBR(string BRLIST)
+        {
+            string datestring = DateTime.Now.ToString("yyyyMMdd");
+            string datefolder = Server.MapPath("~/userfiles") + "\\docs\\" + datestring + "\\";
+
+            if (!Directory.Exists(datefolder))
+            {
+                Directory.CreateDirectory(datefolder);
+            }
+
+            var updatedfolder = datefolder + "agileupdated";
+            var newqueryfolder = datefolder + "agilenewqueried";
+            if (!Directory.Exists(newqueryfolder))
+            {
+                Directory.CreateDirectory(newqueryfolder);
+            }
+
+            return View();
+        }
+
+        public ActionResult UpdateBR()
+        {
+            string datestring = DateTime.Now.ToString("yyyyMMdd");
+            string datefolder = Server.MapPath("~/userfiles") + "\\docs\\" + datestring + "\\";
+
+            if (!Directory.Exists(datefolder))
+            {
+                Directory.CreateDirectory(datefolder);
+            }
+
+            var updatedfolder = datefolder + "agileupdated";
+
+            if (!Directory.Exists(updatedfolder))
+            {
+                Directory.CreateDirectory(updatedfolder);
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ Nebula/Models/AgileVM.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.IO;
+
+namespace Nebula.Models
+{
+    public class AgileVM
+    {
+        public static string PMLastUpdateTime(string pmname,string FirstTraceTime)
+        {
+            var ret = string.Empty;
+            try
+            {
+                var sql = "select LatestTime from PMUpdateTime where PMName = '<PMName>'";
+                sql = sql.Replace("<PMName>", pmname);
+                var dbret = DBUtility.ExeLocalSqlWithRes(sql);
+                if (dbret.Count > 0)
+                {
+                    ret = Convert.ToDateTime(dbret[0][0]).ToString("yyyy-MM-dd hh:mm:ss");
+                }
+            }
+            catch (Exception ex)
+            {
+                ret = FirstTraceTime;
+            }
+
+            if (string.IsNullOrEmpty(ret))
+                ret = FirstTraceTime;
+
+            return ret;
+        }
+
+        public static void RetrieveNewBR(string AGILEURL,string LOCALSITEPORT,string SAVELOCATION,string PMNames,string FirstTraceTime)
+        {
+            var args = "NEWBRQUERY " + AGILEURL + " " + LOCALSITEPORT + " " + SAVELOCATION;
+
+            var pmlist = PMNames.Split(new string[] { ";", "," }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var pm in pmlist)
+            {
+                var tm = PMLastUpdateTime(pm.Trim(), FirstTraceTime);
+                var pmtm = (pm.Trim() + ";;;" + tm.Trim()).Replace(" ", "###");
+                args = args + " " + pmtm;
+            }
+
+            using (Process myprocess = new Process())
+            {
+                myprocess.StartInfo.FileName = Path.Combine(System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath, @"Scripts\agiledownloadwraper\BRAgileDownload.exe").Replace("\\", "/");
+                myprocess.StartInfo.Arguments = args;
+                myprocess.StartInfo.CreateNoWindow = true;
+                myprocess.Start();
+            }
+        }
+
+        public static void UpdateExistBR(string AGILEURL, string LOCALSITEPORT, string SAVELOCATION)
+        {
+            var args = "UPDATEBRLIST " + AGILEURL + " " + LOCALSITEPORT + " " + SAVELOCATION;
+
+            using (Process myprocess = new Process())
+            {
+                myprocess.StartInfo.FileName = Path.Combine(System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath, @"Scripts\agiledownloadwraper\BRAgileDownload.exe").Replace("\\", "/");
+                myprocess.StartInfo.Arguments = args;
+                myprocess.StartInfo.CreateNoWindow = true;
+                myprocess.Start();
+            }
+        }
+
+    }
+}
