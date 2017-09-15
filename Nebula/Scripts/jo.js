271b936b591e232f3e586fcff82cc9038314798a
Brad Qiu <brad.qiu@finisar.com>
2017-09-15 11:56:20 +0800

    Add resource for JOInfo page

--- Nebula/Scripts/jo.js
+++ Nebula/Scripts/jo.js
@@ -2,9 +2,31 @@ var JO = function(){
     var show = function(){
        
     }
+
+    var common = function(){
+        //search event
+        $('body').on('mouseenter', '.row-search', function(){
+            $('.search').addClass('hide');
+            $('.search-div').removeClass('hide');
+        })
+
+        $('body').on('mouseleave', '.row-search', function(){
+            if($('#keywords').val() == ''){
+                $('.search').removeClass('hide');
+                $('.search-div').addClass('hide');
+            }
+        })
+
+        $('body').on('click', '.nav-link', function(){
+            $('.nav-link').removeClass('nav-active');
+            $(this).addClass('nav-active');
+        })
+    }
+
     return {
         init: function () {
             show();
+            common();
         }
     };
 }();
